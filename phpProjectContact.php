df7f7cc6434303a80f36ff6415b455fb1635700d
unknown <abe12002@byui.edu>
2015-06-04 17:39:49 -0600

    Basic email

--- phpProjectContact.php
+++ phpProjectContact.php
@@ -29,40 +29,16 @@
   </div>
 </nav>
 
+
 <?php
-$to = "mistajeffles@gmail.com";
-$subject = "Clark Center Fires Contact";
+// the message
+$msg = "First line of text\nSecond line of text";
 
-$message = "
-<html>
-<head>
-<title>HTML email</title>
-</head>
-<body>
-<p>This email contains HTML Tags!</p>
-<table>
-<tr>
-<th>Firstname</th>
-<th>Lastname</th>
-</tr>
-<tr>
-<td>John</td>
-<td>Doe</td>
-</tr>
-</table>
-</body>
-</html>
-";
+// use wordwrap() if lines are longer than 70 characters
+$msg = wordwrap($msg,70);
 
-// Always set content-type when sending HTML email
-$headers = "MIME-Version: 1.0" . "\r\n";
-$headers .= "Content-type:text/html;charset=UTF-8" . "\r\n";
-
-// More headers
-$headers .= 'From: <mhansen@mattymatt.com>' . "\r\n";
-$headers .= 'Cc: myboss@example.com' . "\r\n";
-
-mail($to,$subject,$message,$headers);
+// send email
+mail("mistajeffles@gmail.com","My subject",$msg);
 ?>
 
 
