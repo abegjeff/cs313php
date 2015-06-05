de98c60a094450161eb8f8edc76ad4d017413419
unknown <abe12002@byui.edu>
2015-06-05 14:08:36 -0600

    Updated mailto and contact info

--- phpProjectContact.php
+++ phpProjectContact.php
@@ -32,79 +32,23 @@
   </div>
 </nav>
 
+  <fieldset>
+   <legend>Owner: Roland Clark</legend>
+   </fieldset>
+   
+   <fieldset>
+   <legend>Email: ClarksCenterFires@ida.net</legend>
+   </fieldset>
+   
+   <fieldset>
+   <legend>Phone Number: (208) 522-8811 </legend>
+   </fieldset>
+   
+<div id="center">
+	<a href="mailto:mistajeffles@gmail.com">Contact Us</a>
+</div>
 
-<form action="mailto:someone@example.com" method="post" enctype="text/plain">
-<fieldset>
-   <legend>Name:</legend>
-	 <input  type="text" name="from" size="20"><br /><br/>
-   <legend>Email:</legend>
-	 <input type="text" name="email" size="20"><br>
-   <legend>Message:</legend>
-	<textarea rows="4" cols="50" name="message">
-	</textarea>
- </fieldset>
-<input type="submit" value="Send">
-<input type="reset" value="Reset">
-</form>
 
-
-<?php
-/*
-if (!empty($_POST['from']) && !empty($_POST['message']))
-{
-
-	$from = $_POST['from'];
-	$message = $_POST['message'];
-	$to = 'jeffabagelen@hotmail.com'; 
-	$WAGLOBAL_Email_Server = "" ;
-	
-	
-	
-	
-	ini_set('sendmail_from', 'mistajeffles@gmail.com');
-	ini_set("SMTP","smtp.example.com" ); 
-
-	
-	$Name = "Da Duder"; //senders name 
-	$email = "jeffabagelen@hotamil.com"; //senders e-mail adress 
-	$recipient = "mistajeffles@gmail.com"; //recipient 
-	$mail_body = "Hello Email World!"; //mail body 
-	$subject = "Test Email"; //subject 
-	$header = "From: ". $Name . " <" . $email . ">\r\n"; //optional headerfields 
-
-	mail($recipient, $subject, $mail_body, $header); //mail command :) 
-	/*
-	echo $message . "<br/>";
-	// the message
-	$msg = "First line of text\nSecond line of text";
-
-// use wordwrap() if lines are longer than 70 characters
-	$msg = wordwrap($msg,70);
-
-// send email
-
-	$subject = 'the subject';
-	$headers = 'From: webmaster@example.com' . "\r\n" .
-       'Reply-To: webmaster@example.com';
-
-	mail($to, $subject, $message, $headers);
-
-	//mail($to,"My subject",$message);
-	
-	echo $from . "<br/>";
-	echo $message;
-}
-/*
-else
-{
-	echo '<script language="javascript">';
-	echo "alert(\"Please fill in all required fields\")";	
-	echo '</script>';
-	
-}
-*/
-
-?>
 
 
 
